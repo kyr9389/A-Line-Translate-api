67de90f8e5081dce0afd17602b5aa97a32dee756
yongrae Kim <kyr9389@naver.com>
2020-12-30 17:14:31 +0900

    [update] API completly applied
    Signed-off-by: yongrae Kim <kyr9389@naver.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const client_id = '***';
+const client_secret = '***';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/translate', function(req, res) {
+    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+    var request = require('request');
+    var options = {
+        url: api_url,
+        form: {'source':'en', 'target':'ko', 'text':req.query.sourceText},
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.post(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
 });
 
 module.exports = router;
